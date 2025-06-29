0d6de15b21a5dde8180c96ca9fab47f335c3aac5
quh4gko8 <88831734+quh4gko8@users.noreply.github.com>
2025-06-29 17:15:59 +0000

    fix BUILD_DATETIME and BUILD_NUMBER for prep builds

--- scripts/make-prep-build.sh
+++ scripts/make-prep-build.sh
@@ -3,5 +3,8 @@
 set -e
 source build/envsetup.sh
 export OFFICIAL_BUILD=true
+# Equivalent to 2025-06-29 12:00 UTC+0
+export BUILD_DATETIME=1751155200
+export BUILD_NUMBER=2025062900
 lunch ${1}-cur-user
 m
